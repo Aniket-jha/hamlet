7243fad97db4824362d35c95762634aae532ca3f
Aniket Jha <aniketjha@Anikets-Laptop.local>
2025-04-07 01:30:43 +0530

    Added Form

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -29,7 +29,7 @@ const Details = ({propertyDetail}) => {
         
         <h1 className='font-[NeueBold]  text-[1.5rem] leading-[1.5rem] lg:leading-[3.5rem]  uppercase lg:text-[2.5rem]' >Discover Comfort and Community at Pune's Premier Coliving Space</h1>
         <p className='font-[NeueRegular]  text-[0.8rem]  lg:text-[1.1rem] ' > Experience the perfect blend of comfort and community at Pune's premier coliving space. Enjoy modern amenities, vibrant social events, and a convenient location, all designed to enhance your living experience. Join us today!</p>
-        <a target='_blank' rel='noreferrer' href="https://form.jotform.com/241691749314462" > <button className="uppercase flex  lg:mt-4 mt-2 items-center justify-center gap-3 rounded-lg shadow-md  font-[NeueMedium] ease-in duration-300    hover:shadow-none hover:bg-[#272727] tracking-wider text-[10px] py-3 px-6 md:text-[16px] md:py-[12px] md:px-[24px] text-[#FFFEFC] bg-[#D64C27]" > Book Now </button></a>
+        <a target='_blank' rel='noreferrer' href="https://unionliving.in/form" > <button className="uppercase flex  lg:mt-4 mt-2 items-center justify-center gap-3 rounded-lg shadow-md  font-[NeueMedium] ease-in duration-300    hover:shadow-none hover:bg-[#272727] tracking-wider text-[10px] py-3 px-6 md:text-[16px] md:py-[12px] md:px-[24px] text-[#FFFEFC] bg-[#D64C27]" > Book Now </button></a>
       </div>
       <div className='border-t-[2px] lg:border-t-[0px] lg:border-l-[2px] pl-0 py-6 lg:py-0 lg:pl-12 border-gray-400' >
       <h1 className='font-[NeueBold]  text-[1.5rem] leading-[1.5rem] lg:leading-[3.5rem]  uppercase lg:text-[2.5rem]' >Amenities</h1>
--- src/Components/WhatSection.js
+++ src/Components/WhatSection.js
@@ -8,7 +8,7 @@ const WhatSection = () => {
         <div>
         <h4 className='font-[NeueBold] leading-[2.4rem] lg:leading-[3.5rem] text-[2rem] lg:text-[3rem]' >What’s missing is you, <br /> Everything else is ready.</h4>
         <p className='font-[NeueRegular] mt-3 mb-6 text-[1rem] lg:text-[1.3rem]' >Settling in can be overwhelming. With big & little things <br className='hidden lg:block' /> already taken care of,  you will be at ease throughout.</p>
-        <a target='_blank' rel='noreferrer' href='https://www.unionliving.in/#/form'> <button className="uppercase mt-6 flex items-center justify-center gap-3 rounded-lg shadow-md  font-[NeueMedium] ease-in duration-300  text-[10px] md:text-[14px]  hover:shadow-none hover:bg-[#272727] tracking-wider py-[6px] px-[24px] md:py-[6px] md:px-[24px] text-[#FFFEFC] bg-[#D64C27]" >Book Now <HiArrowLongRight className="text-[34px]" /> </button></a>
+        <a target='_blank' rel='noreferrer' href='https://unionliving.in/form'> <button className="uppercase mt-6 flex items-center justify-center gap-3 rounded-lg shadow-md  font-[NeueMedium] ease-in duration-300  text-[10px] md:text-[14px]  hover:shadow-none hover:bg-[#272727] tracking-wider py-[6px] px-[24px] md:py-[6px] md:px-[24px] text-[#FFFEFC] bg-[#D64C27]" >Book Now <HiArrowLongRight className="text-[34px]" /> </button></a>
         </div>
         <div className='lg:flex mt-6 lg:mt-0 items-center justify-end' >
             <img className='lg:w-[500px]' src={Checklist} />
